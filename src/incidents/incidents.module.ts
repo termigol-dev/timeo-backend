b35c79344f156db4ecff4f3ca90f8334fa3a26b3
Pablo Esteban <termigol82@gmail.com>
2026-01-09 22:24:06 +0100

    pre deploy

--- src/incidents/incidents.module.ts
+++ src/incidents/incidents.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { IncidentsService } from './incidents.service';
 import { IncidentsController } from './incidents.controller';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { SchedulesModule } from '../schedules/schedules.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    SchedulesModule, // ⬅️ ESTO ES LO QUE FALTABA
+  ],
   controllers: [IncidentsController],
-  providers: [IncidentsService, PrismaService],
+  providers: [IncidentsService],
   exports: [IncidentsService],
 })
 export class IncidentsModule {}
