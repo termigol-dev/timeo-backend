eb1c3917c66e91716d6dcd9f7343d91af0a7fe10
Pablo Esteban <termigol82@gmail.com>
2026-02-11 01:30:43 +0100

    pre deploy

--- src/records/records.controller.ts
+++ src/records/records.controller.ts
@@ -1,40 +1,45 @@
 import { Controller, Post, Get, Req, UseGuards } from '@nestjs/common';
 import { RecordsService } from './records.service';
 import { JwtGuard } from '../auth/guards/jwt.guard';
+import { PunchService } from './punch.service';
+import { RecordType } from '@prisma/client';
 
 @Controller('records')
 @UseGuards(JwtGuard)
 export class RecordsController {
-  constructor(private readonly recordsService: RecordsService) {}
+  constructor(
+    private readonly recordsService: RecordsService,
+    private readonly punchService: PunchService,
+  ) {}
 
   @Post('in')
-  recordIn(@Req() req: any) {
-    const user = req.user;
-
-    return this.recordsService.recordIn(
-      user.id,
-      user.companyId,
-      user.branchId,
-    );
+  recordIn(@Req() req) {
+    return this.punchService.punch({
+      userId: req.user.id,
+      companyId: req.user.companyId,
+      branchId: req.user.branchId,
+      type: RecordType.IN,
+      createdBy: 'MOBILE',
+    });
   }
 
   @Post('out')
-  recordOut(@Req() req: any) {
-    const user = req.user;
-
-    return this.recordsService.recordOut(
-      user.id,
-      user.companyId,
-      user.branchId,
-    );
+  recordOut(@Req() req) {
+    return this.punchService.punch({
+      userId: req.user.id,
+      companyId: req.user.companyId,
+      branchId: req.user.branchId,
+      type: RecordType.OUT,
+      createdBy: 'MOBILE',
+    });
   }
 
   // 📜 HISTORIAL DEL USUARIO LOGUEADO
   @Get('me')
-getMyHistory(@Req() req) {
-  return this.recordsService.getHistory(
-    req.user.id,
-    req.user.companyId,
-  );
-}
+  getMyHistory(@Req() req) {
+    return this.recordsService.getHistory(
+      req.user.id,
+      req.user.companyId,
+    );
+  }
 }
