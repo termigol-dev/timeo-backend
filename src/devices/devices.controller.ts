e0a27422370a23600c0f17d15caa89103fc8a72b
Pablo Esteban <termigol82@gmail.com>
2026-03-16 21:03:30 +0100

    pre deploy

--- src/devices/devices.controller.ts
+++ src/devices/devices.controller.ts
@@ -3,19 +3,34 @@ import { DevicesService } from './devices.service';
 
 @Controller('devices')
 export class DevicesController {
-  constructor(private devicesService: DevicesService) { }
+  constructor(private devicesService: DevicesService) {}
 
   @Post('register')
   async registerDevice(
     @Body() body: { token: string; platform: 'WEB' | 'ANDROID' | 'IOS' },
     @Req() req: any,
   ) {
-    const userId = req.user.id;
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new Error('Missing Authorization header');
+    }
+
+    const jwt = authHeader.split(' ')[1];
+
+    const payload = JSON.parse(
+      Buffer.from(jwt.split('.')[1], 'base64').toString()
+    );
+
+    const userId = payload.sub;
+
     console.log('REGISTER DEVICE', {
       userId,
       token: body.token,
       platform: body.platform
     });
+
     return this.devicesService.registerDevice(
       userId,
       body.token,
