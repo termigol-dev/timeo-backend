48e225e1db36a5cc4f0fbffe1949f3e94a816fc6
Pablo Esteban <termigol82@gmail.com>
2026-03-16 23:55:10 +0100

    pre deploy

--- src/devices/devices.controller.ts
+++ src/devices/devices.controller.ts
@@ -1,28 +1,31 @@
-import { Body, Controller, Post, Req, Get, Param } from '@nestjs/common';
+import { Body, Controller, Post, Req, Get, Param, UseGuards } from '@nestjs/common';
 import { DevicesService } from './devices.service';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Controller('devices')
 export class DevicesController {
+
   constructor(private devicesService: DevicesService) {}
 
+  @UseGuards(JwtGuard)
   @Post('register')
-async registerDevice(
-  @Body() body: { token: string; platform: 'WEB' | 'ANDROID' | 'IOS' },
-  @Req() req: any,
-) {
+  async registerDevice(
+    @Body() body: { token: string; platform: 'WEB' | 'ANDROID' | 'IOS' },
+    @Req() req: any,
+  ) {
 
-  const userId = req.user.id;
+    const userId = req.user.id;
 
-  console.log("DEVICE REGISTER REQUEST:");
-  console.log("USER:", userId);
-  console.log("TOKEN:", body.token);
+    console.log("DEVICE REGISTER REQUEST");
+    console.log("USER:", userId);
+    console.log("TOKEN:", body.token.substring(0, 80));
 
-  return this.devicesService.registerDevice(
-    userId,
-    body.token,
-    body.platform,
-  );
-}
+    return this.devicesService.registerDevice(
+      userId,
+      body.token,
+      body.platform,
+    );
+  }
 
   @Get('user/:userId')
   async getDevicesByUser(@Param('userId') userId: string) {
