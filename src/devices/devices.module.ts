a6abfb12ea1da1a01db62487937ac1fec8fe3f18
Pablo Esteban <termigol82@gmail.com>
2026-03-16 21:50:40 +0100

    pre deploy

--- src/devices/devices.module.ts
+++ src/devices/devices.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [DevicesController],
   providers: [DevicesService, PrismaService],
+  imports: [PrismaModule],
+  exports: [DevicesService],   //
 })
 export class DevicesModule {}
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { DevicesModule } from '../devices/devices.module';
+import { NotificationsController } from './notifications.controller';
 
 @Module({
   imports: [DevicesModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
+  controllers: [NotificationsController],
 })
 export class NotificationsModule {}
