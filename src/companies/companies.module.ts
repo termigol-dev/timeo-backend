abbc14c64aa70c63ab9f1bee5420394b658b477a
Pablo Esteban <termigol82@gmail.com>
2026-05-01 19:19:22 +0200

    pre deploy

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,JwtModule.register({})],
   controllers: [CompaniesController],
   providers: [CompaniesService],
 })
