717c74fb5ecd9a3676b8fdee04c4590be9f35e85
Pablo Esteban <termigol82@gmail.com>
2026-05-04 13:06:54 +0200

    pre deploy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { UsersModule } from '../users/users.module';
     UsersModule,
     PrismaModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'dev_secret',
+      secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '1d' },
     }),
   ],
