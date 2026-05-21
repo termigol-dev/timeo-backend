d27d9971e3d1be9baa6dc3492407ef8080f71816
Pablo Esteban <termigol82@gmail.com>
2026-05-21 18:06:21 +0200

    pre deploy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -64,7 +64,7 @@ export class AuthService {
 
     const payload = {
       sub: user.id,
-      email:user.email,
+      email: user.email,
       role: membership?.role ?? 'NO_ROLE',
       companyId: membership?.companyId ?? null,
       branchId: membership?.branchId ?? null,
@@ -73,31 +73,61 @@ export class AuthService {
     const token = this.jwt.sign(payload);
 
     let companyName: string | null = null;
+    let companyStatus: string | null = null;
 
     if (membership?.companyId) {
-      const company = await this.prisma.company.findUnique({
-        where: { id: membership.companyId },
-        select: { commercialName: true },
-      });
 
-      companyName = company?.commercialName ?? null;
+      const company =
+        await this.prisma.company.findUnique({
+
+          where: {
+            id: membership.companyId,
+          },
+
+          select: {
+            commercialName: true,
+            subscriptionStatus: true,
+          },
+        });
+
+      companyName =
+        company?.commercialName ?? null;
+
+      companyStatus =
+        company?.subscriptionStatus ?? null;
     }
 
     const response = {
+
       token,
+
       user: {
+
         id: user.id,
+
         name: user.name,
-        lastName: user.firstSurname ?? null,
+
+        lastName:
+          user.firstSurname ?? null,
+
         email: user.email,
+
         role: payload.role,
-        companyId: payload.companyId,
-        branchId: payload.branchId,
+
+        companyId:
+          payload.companyId,
+
+        branchId:
+          payload.branchId,
+
         companyName,
-        photoUrl: user.photoUrl ?? null,
+
+        companyStatus,
+
+        photoUrl:
+          user.photoUrl ?? null,
       },
     };
-
     console.log('✅ LOGIN RESPONSE (BACKEND):', response);
 
     return response;
@@ -115,90 +145,85 @@ export class AuthService {
   }
 
   async getMe(userId: string) {
-  console.log('🔥 GET ME EJECUTÁNDOSE');
-  const membership =
-    await this.prisma.membership.findFirst({
+    console.log('🔥 GET ME EJECUTÁNDOSE');
+    const membership =
+      await this.prisma.membership.findFirst({
 
-      where: {
-        userId,
-        active: true,
-      },
+        where: {
+          userId,
+          active: true,
+        },
 
-      include: {
-        company: true,
-      },
-    });
+        include: {
+          company: true,
+        },
+      });
 
-  const user =
-    await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-
-  /*
-  |--------------------------------------------------------------------------
-  | AUTO EXPIRE TRIAL
-  |--------------------------------------------------------------------------
-  */
-
-  if (
-    membership?.company &&
-    shouldExpireTrial(
-      membership.company
-    )
-  ) {
-
-    await this.prisma.company.update({
-
-      where: {
-        id: membership.company.id,
-      },
-
-      data: {
-        subscriptionStatus:
-          'EXPIRED',
-      },
-    });
-
-    membership.company.subscriptionStatus =
-      'EXPIRED';
-  }
-
-  return {
-
-    id: user.id,
-
-    name: user.name,
-
-    email: user.email,
-
-    companyId:
-      membership?.companyId || null,
-
-    companyName:
-      membership?.company
-        ?.commercialName || null,
-
-    role:
-      membership?.role || 'NO_ROLE',
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+      });
 
     /*
     |--------------------------------------------------------------------------
-    | BILLING
+    | AUTO EXPIRE TRIAL
     |--------------------------------------------------------------------------
     */
 
-    companyStatus:
-      membership?.company
-        ?.subscriptionStatus || null,
+    if (
+      membership?.company &&
+      shouldExpireTrial(
+        membership.company
+      )
+    ) {
 
-    canOperate:
-      isCompanyOperational(
+      await this.prisma.company.update({
+
+        where: {
+          id: membership.company.id,
+        },
+
+        data: {
+          subscriptionStatus:
+            'EXPIRED',
+        },
+      });
+
+      membership.company.subscriptionStatus =
+        'EXPIRED';
+    }
+
+    return {
+
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      companyId:
+        membership?.companyId || null,
+      companyName:
         membership?.company
-      ),
-  };
-}
+          ?.commercialName || null,
+      role:
+        membership?.role || 'NO_ROLE',
+
+      /*
+      |--------------------------------------------------------------------------
+      | BILLING
+      |--------------------------------------------------------------------------
+      */
+
+      companyStatus:
+        membership?.company
+          ?.subscriptionStatus || null,
+
+      canOperate:
+        isCompanyOperational(
+          membership?.company
+        ),
+    };
+  }
 
   async register(body: any) {
     const user = await this.usersService.registerCompanyAdmin(body);
